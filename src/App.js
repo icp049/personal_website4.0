23bde87280419522a0653bcb41b5eef8cf66d5fe
icp049 <ianpepe2000@gmail.com>
2023-06-16 00:00:53 -0600

    modified again

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
-
+import Resume from './components/Resume'
 import Projects from './components/Projects'
 
 
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
       <About />
       <Projects />
-     
+      <Resume/ >
     </div>
   );
 }
