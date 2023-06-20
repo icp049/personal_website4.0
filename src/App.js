fa44e8adab59e8d7791dabb8a1f22db206a2ee32
icp049 <ianpepe2000@gmail.com>
2023-06-19 23:06:36 -0600

    just minor tweaks

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Projects from './components/Projects'
 import Footer from './components/Footer.js'
 import ImaheLaunch from './components/ImaheLaunch.js'
 
+
 function App() {
   return (
     <div>
