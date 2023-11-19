9fcf5921f4d68435197c9a8110e57786b06b5d4f
icp049 <ianpepe2000@gmail.com>
2023-11-18 21:12:31 -0600

    changed teh close button to x and placed it in the upper right corner

--- src/components/ImaheModal.js
+++ src/components/ImaheModal.js
@@ -1,3 +1,5 @@
+// ImaheModal.js
+
 import React from "react";
 import "./ImaheModal.css";
 
@@ -5,10 +7,12 @@ const ImaheModal = ({ onClose }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
+        <span className="close-button" onClick={onClose}>
+          &times;
+        </span>
         {/* Add your modal content here */}
         <h2>Imahe Modal</h2>
         <p>This is the modal content.</p>
-        <button onClick={onClose}>Close</button>
       </div>
     </div>
   );
