4705b925905ce947134cb2dd9337e13003aa31aa
icp049 <ianpepe2000@gmail.com>
2023-12-15 21:34:31 -0600

    prototyping app policy page

---
+++ src/components/HardPassPolicy.js
@@ -0,0 +1,36 @@
+import react from "react";
+
+
+const HardPassPolicy = () => {
+    return(
+
+
+        <div className = {styles.PolicyContainer}>
+
+        <div className = {styles.apptitleContainer}>
+
+        <h1>HardPass</h1>
+        </div>
+        
+
+
+        <div className = {styles.screenshotContainer}>
+
+
+
+
+        </div>
+
+
+        <div className = {styles.policyContainer}>
+        <p> </p>
+        </div>
+         
+
+
+
+        </div>
+    )
+}
+
+export default HardPassPolicy;
